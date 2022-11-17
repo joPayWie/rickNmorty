a31a5b580a051ce5700285360dde3ee5e5098453
joPay <josefina.payer@gmail.com>
2022-11-16 22:02:05 -0300

    rick n morty

---
+++ script.js
@@ -0,0 +1,119 @@
+const $ = (selector) => document.querySelector(selector)
+const $$ = (selector) => document.querySelectorAll(selector)
+
+const BASE_URL = "https://rickandmortyapi.com/api/"
+
+let page = 1
+
+let totalPages
+
+const getCharacters = async (page, endpoint = "") => {
+    const response = await fetch(`${BASE_URL}character/?page=${page}${endpoint}`)
+    const info = await response.json()
+    return info
+}
+
+getCharacters(page).then(data => { 
+    renderCharacters(data.results)
+    totalPages = data.info.pages
+}).catch(err => {   
+    $("#cards").innerHTML = "" 
+    $("#error-p").innerHTML = "No se encontraron resultados"
+})
+
+const renderCharacters = (characters) => {
+    if (characters.length !== 0) {
+        $("#error-p").innerHTML = ""
+        $("#cards").innerHTML = ""
+        for (const { id, gender, image, name, species, status } of characters) {
+            $("#cards").innerHTML += `
+                <div class="card" style="width: 18rem;">
+                    <img src="${image}" class="card-img-top" alt="Avatar de ${name}">
+                    <div class="card-body">
+                        <h5 class="card-title">${name}</h5>
+                        <p class="card-text">Gender: ${gender}</p>
+                        <p class="card-text">Status: ${status}</p>
+                        <p class="card-text">Species: ${species}</p>
+                        <a href="#" class="btn btn-primary" onclick="getCharacter(${id})">Ver detalles</a>
+                    </div>
+                </div>
+            `
+        }
+    } else {
+        $("#cards").innerHTML = ""
+        $("#error-p").innerHTML = "No se encontraron resultados"
+    }
+}
+
+const radioBtnChecked = (radioBtns) => {
+    for (const btn of radioBtns) {
+        if (btn.checked) {
+            return btn.value
+        }
+    }
+}
+
+const filterChar = () => {
+    return `&name=${$("#character-name").value}&status=${radioBtnChecked($$(".status-radio")) ? radioBtnChecked($$(".status-radio")) : ''}&gender=${radioBtnChecked($$(".gender-radio")) ? radioBtnChecked($$(".gender-radio")) : ''}`
+}
+
+
+$("form").addEventListener("submit", (e) => {
+    e.preventDefault()
+    getCharacters(page, filterChar()).then(data => renderCharacters(data.results)).catch(err => {   
+        $("#cards").innerHTML = "" 
+        $("#error-p").innerHTML = "No se encontraron resultados"
+    })
+})
+
+$("#btn-reset").addEventListener("click", () => {
+    $("form").reset()
+    $("#cards").innerHTML = ""
+    getCharacters(page).then(data => renderCharacters(data.results)).catch(err => {   
+        $("#cards").innerHTML = "" 
+        $("#error-p").innerHTML = "No se encontraron resultados"
+    })
+})
+
+const nextPage = () => {
+    page = page + 1
+    for ( const prevBtn of $$(".prev") ) {
+        prevBtn.removeAttribute("disabled")
+         }
+    if ( page === totalPages ) {
+        for ( const nextBtn of $$(".next") ) {
+           nextBtn.setAttribute("disabled", true)
+        }
+    }
+}
+
+const prevPage = () => {
+    if ( page > 1 ) {
+        page = page - 1
+    } else {
+        for ( const prevBtn of $$(".prev") ) {
+        prevBtn.setAttribute("disabled", true)
+         }
+    }
+    
+}
+
+for ( const prevBtn of $$(".prev") ) {
+    prevBtn.addEventListener("click", () => {
+        getCharacters(page, filterChar()).then(data => renderCharacters(data.results)).catch(err => {   
+            $("#cards").innerHTML = "" 
+            $("#error-p").innerHTML = "No se encontraron resultados"
+        })
+        prevPage()
+    })
+}
+
+for ( const nextBtn of $$(".next") ) {
+    nextBtn.addEventListener("click", () => {
+        getCharacters(page, filterChar()).then(data => renderCharacters(data.results)).catch(err => {   
+            $("#cards").innerHTML = "" 
+            $("#error-p").innerHTML = "No se encontraron resultados"
+        })
+        nextPage()
+    })
+}
